932ee3c51978d7f8aab7cedc6a81b4cc4e168921
PangZhiYou <pangzhiyou@qmlke.com>
2023-10-07 10:41:22 +0800

    修复不同路径下，类之间的随机调用，v2

--- library/src/main/groovy/cn/hx/plugin/junkcode/utils/MethodsUtil.groovy
+++ library/src/main/groovy/cn/hx/plugin/junkcode/utils/MethodsUtil.groovy
@@ -5,11 +5,8 @@ import com.squareup.javapoet.MethodSpec
 import com.squareup.javapoet.TypeSpec
 
 import javax.lang.model.element.Modifier
-import java.util.logging.Logger
-
 
 class MethodsUtil {
-
 
     static random = new Random()
 
